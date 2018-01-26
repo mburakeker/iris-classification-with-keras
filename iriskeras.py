0dbce7b2d845f506cc5d26c2de60d00d5f950fe6
Mehmet Burak Eker <Escorpioneer@gmail.com>
2018-01-26 03:57:56 +0300

    Update iriskeras.py

--- iriskeras.py
+++ iriskeras.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.preprocessing import LabelEncoder
@@ -17,19 +11,11 @@ df = pd.read_csv("iris.csv")
 X = df.drop(["class"],axis=1)
 Y = df['class'].map({'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2})
 
-
-# In[2]:
-
-
 encoder = LabelEncoder()
 encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
 
-# to_categorical converts the numbered labels into a one-hot vector
 dummy_y = np_utils.to_categorical(encoded_Y)
-
-
-# In[3]:
 
 
 model = Sequential()
@@ -38,34 +24,19 @@ model.add(Dense(3, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
-# In[5]:
+model.fit(X, dummy_y, epochs=150, batch_size=5)
 
-
-estimator = model.fit(X, dummy_y, epochs=150, batch_size=5)
-
-
-# In[6]:
-
-
-# evaluate the model
 scores = model.evaluate(X, dummy_y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 
-# In[10]:
-
 
 predictions = model.predict(X)
 
-
-# In[20]:
-
-
+# 0 = setosa , 1 = versicolor , 2 = virginica
 test = np.argmax(predictions,axis=1)
-test
 
-
-# In[18]:
+print(test)
 
 
 
